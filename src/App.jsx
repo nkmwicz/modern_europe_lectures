ceef0177e01c8156002c26c4c7e80922d614d3e4
Nathan Michalewicz <nathan@mwicz.com>
2023-03-24 17:06:06 -0400

    move arrows to outside router

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import "./App.css";
 import { Router } from "./router/Router";
 import "@nkmwicz/reactPresentation/dist/style.css";
 import { Nav } from "./components/nav";
+import { TheArrows } from "./components/TheArrows";
 
 function App() {
   return (
     <>
+      <TheArrows />
       <Nav />
       <Router />
     </>
