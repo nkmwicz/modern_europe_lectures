d5edbfc43d6f9808926eed6f44bde5f966c17e69
Nathan Michalewicz <nathan@mwicz.com>
2023-03-24 17:06:23 -0400

    add recoilroot

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/teaching/modern-europe/lectures/">
-      <App />
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter basename="/teaching/modern-europe/lectures/">
+        <App />
+      </BrowserRouter>
+    </RecoilRoot>
   </React.StrictMode>
 );
