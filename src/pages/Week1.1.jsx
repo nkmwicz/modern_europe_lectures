6e6d0ef535f42f828dd97a1ec3d075b191a573ae
Nathan Michalewicz <nathan@mwicz.com>
2023-03-24 16:52:41 -0400

    refactor

--- src/pages/Week1.1.jsx
+++ src/pages/Week1.1.jsx
@@ -1,57 +1,8 @@
 import React from "react";
-import { QuickLayout, cacheImages, Spinner } from "@nkmwicz/reactpresentation";
+import { PageTemplate } from "../components/PageTemplate";
 
 export function Week1_1() {
-  const [slides, setSlides] = React.useState([]);
-  const [slide, setSlide] = React.useState({});
-  const [slideIndex, setSlideIndex] = React.useState(0);
-  const [isLoading, setIsLoading] = React.useState(true);
-
-  function nextSlide(e) {
-    if (e && slideIndex < slides.length - 1) {
-      setSlideIndex(slideIndex + 1);
-    }
-    if (e && slideIndex === slides.length - 1) {
-      setSlideIndex(0);
-    }
-  }
-
-  function prevSlide(e) {
-    if (e && slideIndex > 0) {
-      setSlideIndex(slideIndex - 1);
-    }
-    if (e && slideIndex === 0) {
-      setSlideIndex(slides.length - 1);
-    }
-  }
-
-  React.useEffect(() => {
-    setSlide(slides[slideIndex]);
-  }, [slideIndex, slides]);
-
-  React.useEffect(() => {
-    async function fetchSlides() {
-      const res = await fetch(
-        "https://raw.githubusercontent.com/nkmwicz/teach-lectures/master/modern-europe/01-introduction.json"
-      );
-      const data = await res.json();
-      setSlides(data.slides);
-      await cacheImages(data.images);
-      setIsLoading(false);
-    }
-    fetchSlides();
-  }, []);
-
-  if (isLoading) {
-    return <Spinner />;
-  }
-
   return (
-    <QuickLayout
-      mode={"dark"}
-      slide={slide}
-      nextClick={nextSlide}
-      prevClick={prevSlide}
-    />
+    <PageTemplate url="https://raw.githubusercontent.com/nkmwicz/teach-lectures/master/modern-europe/01-introduction.json" />
   );
 }
