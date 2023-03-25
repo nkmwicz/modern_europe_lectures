e80099e76198b83f4400f2939523815f70baf328
Nathan Michalewicz <nathan@mwicz.com>
2023-03-25 09:12:59 -0400

    fix name

--- src/pages/Discuss8.jsx
+++ src/pages/Discuss8.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { PageTemplate } from "../components/PageTemplate";
 
-export function Discussion8() {
+export function Discuss8() {
   return (
     <PageTemplate url="https://raw.githubusercontent.com/nkmwicz/teach-lectures/master/modern-europe/28-discussion-8.json" />
   );
