300efa89893de2c3fa70a55c52abbf8483f0b61c
Nathan Michalewicz <nathan@mwicz.com>
2023-03-25 09:13:06 -0400

    add routes

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,6 +3,43 @@ import { Intro } from "../pages/Intro";
 import { Ren1 } from "../pages/Ren1";
 import { Ren2 } from "../pages/Ren2";
 import { Explor1 } from "../pages/Explor1";
+import { Explor2 } from "../pages/Explor2";
+import { Discuss1 } from "../pages/Discuss1";
+import { Ref } from "../pages/Ref";
+import { RelWar } from "../pages/RelWar";
+import { Discuss2 } from "../pages/Discuss2";
+import { SciRev } from "../pages/SciRev";
+import { Colonial } from "../pages/Colonial";
+import { Discuss3 } from "../pages/Discuss3";
+import { Slave } from "../pages/Slave";
+import { Enlighten } from "../pages/Enlighten";
+import { Discuss4 } from "../pages/Discuss4";
+import { FrRev1 } from "../pages/FrRev1";
+import { FrRev2 } from "../pages/FrRev2";
+import { Discuss5 } from "../pages/Discuss5";
+import { Revs19c } from "../pages/Revs19c";
+import { IndRev } from "../pages/IndRev";
+import { Isms } from "../pages/Isms";
+import { Discuss6 } from "../pages/Discuss6";
+import { Nationalism } from "../pages/Nationalism";
+import { Imperial1 } from "../pages/Imperial1";
+import { Discuss7 } from "../pages/Discuss7";
+import { Imperial2 } from "../pages/Imperial2";
+import { WWI } from "../pages/WWI";
+import { Discuss8 } from "../pages/Discuss8";
+import { WW1Conseq } from "../pages/WW1Conseq";
+import { InterWar1 } from "../pages/InterWar1";
+import { Discuss9 } from "../pages/Discuss9";
+import { InterWar2 } from "../pages/InterWar2";
+import { WWII } from "../pages/WWII";
+import { Discuss10 } from "../pages/Discuss10";
+import { Discuss11 } from "../pages/Discuss11";
+import { Discuss12 } from "../pages/Discuss12";
+import { Holocaust } from "../pages/Holocaust";
+import { ColdWar1 } from "../pages/ColdWar1";
+import { ColdWar2 } from "../pages/ColdWar2";
+import { Decolonize1 } from "../pages/Decolonize1";
+import { Decolonize2 } from "../pages/Decolonize2";
 
 export function Router() {
   return (
@@ -14,7 +51,7 @@ export function Router() {
       <Route path="/exploration2" element={<Explor2 />} />
       <Route path="/discussion1" element={<Discuss1 />} />
       <Route path="/reformation" element={<Ref />} />
-      <Route path="/relwars" element={<RelWars />} />
+      <Route path="/relwars" element={<RelWar />} />
       <Route path="/discussion2" element={<Discuss2 />} />
       <Route path="/scirev" element={<SciRev />} />
       <Route path="/colonialism" element={<Colonial />} />
