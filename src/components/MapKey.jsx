4a216b2c90061ca0b0888010d34748b01061ae9e
Nathan Michalewicz <nathan@mwicz.com>
2023-07-25 14:15:04 -0400

    initial commit

---
+++ src/components/MapKey.jsx
@@ -0,0 +1,109 @@
+import { useRecoilValue } from "recoil";
+import { slideState } from "./globalState";
+import { useState } from "react";
+import { KeySVG } from "./KeySVG";
+
+export function MapKey() {
+  const slide = useRecoilValue(slideState);
+  const [isKeyOpen, setIsKeyOpen] = useState(false);
+  const keyStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "35px",
+    width: "35px",
+    position: "absolute",
+    zIndex: "999",
+    border: "1px solid black",
+    borderRadius: "5px",
+    backgroundColor: "white",
+    bottom: "10px",
+    left: "10px",
+    cursor: "pointer",
+  };
+
+  const divStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    position: "absolute",
+    zIndex: "999",
+    border: "1px solid black",
+    borderRadius: "5px",
+    backgroundColor: "white",
+    bottom: "10px",
+    left: "10px",
+  };
+  const headerStyle = {
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "row",
+    color: "black",
+    fontSize: "24px",
+  };
+  const bodyStyle = {
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    flexWrap: "wrap",
+  };
+  const innerBodyStyle = {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+    color: "black",
+    fontSize: "24px",
+    flexWrap: "no wrap",
+    gap: "1em",
+    padding: "0.5em",
+  };
+
+  const closeBtn = {
+    position: "absolute",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "25px",
+    width: "25px",
+    cursor: "pointer",
+    color: "black",
+    fontSize: "18px",
+    right: "0.6em",
+    top: "5px",
+  };
+  return (
+    <>
+      {isKeyOpen ? (
+        <div style={divStyle}>
+          <div style={headerStyle}>
+            Legend
+            <button style={closeBtn} onClick={() => setIsKeyOpen(false)}>
+              X
+            </button>
+          </div>
+          <div style={bodyStyle}>
+            {Object.entries(slide.stateColors).map(([key, value]) => (
+              <div style={innerBodyStyle}>
+                {key}
+                <div
+                  style={{
+                    backgroundColor: value,
+                    height: "25px",
+                    width: "25px",
+                    borderRadius: "50%",
+                  }}
+                ></div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <button style={keyStyle} onClick={() => setIsKeyOpen(true)}>
+          <KeySVG height={"25px"} width={"25px"} />
+        </button>
+      )}
+    </>
+  );
+}
