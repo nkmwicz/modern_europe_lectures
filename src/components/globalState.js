6da305ac3e33dc37f861d9c37a71b0de571d2f30
Nathan Michalewicz <nathan@mwicz.com>
2023-07-25 10:24:53 -0400

    add map state

--- src/components/globalState.js
+++ src/components/globalState.js
@@ -23,3 +23,23 @@ export const locationState = atom({
   key: "locationState",
   default: "Introduction",
 });
+
+export const mapCenterState = atom({
+  key: "mapCenterState",
+  default: [41.9, 12.5],
+});
+
+export const map1900State = atom({
+  key: "map1900State",
+  default: [],
+});
+
+export const map1914State = atom({
+  key: "map1914State",
+  default: [],
+});
+
+export const map1920State = atom({
+  key: "map1920State",
+  default: [],
+});
