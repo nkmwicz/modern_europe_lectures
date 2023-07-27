d16f50c7abd2e10204e4c32b09e5bb2928740f63
Nathan Michalewicz <nathan@mwicz.com>
2023-07-27 16:16:38 -0400

    ADD maps

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,23 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Polygon, Popup, useMap } from "react-leaflet";
+import { useRecoilValue } from "recoil";
+import { map1900State, map1914State, slideState } from "./globalState";
+import { useEffect, useMemo } from "react";
+import { MapLabel } from "./MapLabel";
+import { MapBorders } from "./MapBorders";
+import { MapKey } from "./MapKey";
+import { MapLatLngIndicator } from "./MapLatLngIndicator";
 
 export function Map({ mapCenter }) {
+  const slide = useRecoilValue(slideState);
+
   return (
-    <MapContainer center={mapCenter} scrollWheelZoom={false} zoom={13}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-    </MapContainer>
+    <>
+      <MapContainer center={mapCenter} className="mymap" zoom={5}>
+        <MapBorders />
+        <MapKey />
+        <MapLatLngIndicator />
+      </MapContainer>
+      <MapLabel />
+    </>
   );
 }
