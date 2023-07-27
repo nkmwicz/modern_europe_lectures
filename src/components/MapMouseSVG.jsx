ecc52002313cc23c8f5880c4615b8ff5bb0863dc
Nathan Michalewicz <nathan@mwicz.com>
2023-07-27 16:17:39 -0400

    resize svg

--- src/components/MapMouseSVG.jsx
+++ src/components/MapMouseSVG.jsx
@@ -2,8 +2,8 @@ export function MapMouseSVG() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="800px"
-      height="800px"
+      width="20px"
+      height="20px"
       viewBox="0 0 24 24"
       fill="none"
     >
