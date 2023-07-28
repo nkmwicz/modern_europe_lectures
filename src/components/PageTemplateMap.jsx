6705f1514db5c1556b81c096137d4a17042348d4
Nathan Michalewicz <nathan@mwicz.com>
2023-07-28 12:10:50 -0400

    add parameters to url

--- src/components/PageTemplateMap.jsx
+++ src/components/PageTemplateMap.jsx
@@ -12,11 +12,18 @@ import {
   slideState,
 } from "../components/globalState";
 import { Map } from "./Map";
+import { useSearchParams } from "react-router-dom";
 
 export function PageTemplateMap({ url, mapCenter }) {
   const [slides, setSlides] = useRecoilState(allSlides);
   const slide = useRecoilValue(slideState);
+  const [slideIndex, setSlideIndex] = useRecoilState(slideIndexState);
   const [isLoading, setIsLoading] = React.useState(true);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const param = searchParams.get("slideIndex");
+  const isNumber = (value) => {
+    return /^-?\d*\.?\d+$/.test(value);
+  };
 
   React.useEffect(() => {
     async function fetchSlides() {
@@ -25,9 +32,38 @@ export function PageTemplateMap({ url, mapCenter }) {
       setSlides(data.slides);
       await cacheImages(data.images);
       setIsLoading(false);
+      if (param === null || param === undefined) {
+        setSlideIndex(0);
+        setSearchParams({ slideIndex: slideIndex });
+        return;
+      }
+      if (param > data.slides.length - 1) {
+        setSlideIndex(data.slides.length - 1);
+        setSearchParams({ slideIndex: data.slides.length - 1 });
+        return;
+      }
+      if (param < 0) {
+        setSlideIndex(0);
+        setSearchParams({ slideIndex: 0 });
+        return;
+      }
+      if (!isNumber(param)) {
+        setSlideIndex(parseInt(0));
+        setSearchParams({ slideIndex: 0 });
+        return;
+      }
+      if (param !== null || param !== undefined) {
+        setSlideIndex(parseInt(param));
+        setSearchParams({ slideIndex: param });
+        return;
+      }
     }
     fetchSlides();
   }, []);
+
+  React.useEffect(() => {
+    setSearchParams({ slideIndex: slideIndex });
+  }, [slideIndex]);
 
   if (isLoading) {
     return <Spinner />;
